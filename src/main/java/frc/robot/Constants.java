3b166e9e337bb367a7845a2feae093f9f3b529aa
Floyd E Moore <floyd.moore@hp.com>
2023-10-26 09:25:18 -0700

    cleanup and udate

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,159 +22,139 @@ import frc.robot.utils.SwerveModuleConstants;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static final class DriveConstants {
-    public static final int kFrontLeftDriveMotorPort = 0;
-    public static final int kRearLeftDriveMotorPort = 2;
-    public static final int kFrontRightDriveMotorPort = 4;
-    public static final int kRearRightDriveMotorPort = 6;
+    public static final class DriveConstants {
+        public static final double kTrackWidth = 0.5;
+        // Distance between centers of right and left wheels on robot
+        public static final double kWheelBase = 0.7;
+        // Distance between front and back wheels on robot
+        public static final SwerveDriveKinematics kDriveKinematics =
+                new SwerveDriveKinematics(
+                        new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+                        new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+                        new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+                        new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
-    public static final int kFrontLeftTurningMotorPort = 1;
-    public static final int kRearLeftTurningMotorPort = 3;
-    public static final int kFrontRightTurningMotorPort = 5;
-    public static final int kRearRightTurningMotorPort = 7;
+        public static final boolean kGyroReversed = false;
 
-    public static final boolean kFrontLeftTurningEncoderReversed = false;
-    public static final boolean kRearLeftTurningEncoderReversed = true;
-    public static final boolean kFrontRightTurningEncoderReversed = false;
-    public static final boolean kRearRightTurningEncoderReversed = true;
+        // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
+        // These characterization values MUST be determined either experimentally or theoretically
+        // for *your* robot's drive.
+        // The SysId tool provides a convenient method for obtaining these values for your robot.
+        public static final double ksVolts = 1;
+        public static final double kvVoltSecondsPerMeter = 0.8;
+        public static final double kaVoltSecondsSquaredPerMeter = 0.15;
 
-    public static final boolean kFrontLeftDriveEncoderReversed = false;
-    public static final boolean kRearLeftDriveEncoderReversed = true;
-    public static final boolean kFrontRightDriveEncoderReversed = false;
-    public static final boolean kRearRightDriveEncoderReversed = true;
+        public static final double kMaxSpeedMetersPerSecond = 4.4;
+    }
 
-    public static final double kTrackWidth = 0.5;
-    // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = 0.7;
-    // Distance between front and back wheels on robot
-    public static final SwerveDriveKinematics kDriveKinematics =
-        new SwerveDriveKinematics(
-            new Translation2d(kWheelBase / 2, kTrackWidth / 2),
-            new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
-            new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
-            new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+    public static final class ModuleConstants {
 
-    public static final boolean kGyroReversed = false;
+        public static final double kMaxModuleAngularSpeedRadiansPerSecond = 2 * Math.PI;
+        public static final double kMaxModuleAngularAccelerationRadiansPerSecondSquared = 2 * Math.PI;
 
-    // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
-    // These characterization values MUST be determined either experimentally or theoretically
-    // for *your* robot's drive.
-    // The SysId tool provides a convenient method for obtaining these values for your robot.
-    public static final double ksVolts = 1;
-    public static final double kvVoltSecondsPerMeter = 0.8;
-    public static final double kaVoltSecondsSquaredPerMeter = 0.15;
+        public static final int kEncoderCPR = 1024;
+        public static final double kWheelDiameterMeters = 0.15;
+        public static final double kDriveEncoderDistancePerPulse =
+                // Assumes the encoders are directly mounted on the wheel shafts
+                (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
 
-    public static final double kMaxSpeedMetersPerSecond = 4.4;
-  }
+        public static final double kTurningEncoderDistancePerPulse =
+                // Assumes the encoders are on a 1:1 reduction with the module shaft.
+                (2 * Math.PI) / (double) kEncoderCPR;
 
-  public static final class ModuleConstants {
+        /** Constants that apply to the whole drive train. */
+        public static final double TRACK_WIDTH = Units.inchesToMeters(24.0); // Width of the drivetrain measured from the middle of the wheels.
+        public static final double WHEEL_BASE = Units.inchesToMeters(24.0); // Length of the drivetrain measured from the middle of the wheels.
+        public static final double WHEEL_DIAMETER = Units.inchesToMeters(4);
+        public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
 
-    public static final double kMaxModuleAngularSpeedRadiansPerSecond = 2 * Math.PI;
-    public static final double kMaxModuleAngularAccelerationRadiansPerSecondSquared = 2 * Math.PI;
+        // SDS Mk4i L2 Drive Ratio = 6.75:1, Angle Ratio = 150/7:1
+        public static final double DRIVE_GEAR_RATIO = 6.75 / 1.0; // 6.75:1
+        public static final double DRIVE_ROTATIONS_TO_METERS = WHEEL_CIRCUMFERENCE / DRIVE_GEAR_RATIO;
+        public static final double DRIVE_RPM_TO_METERS_PER_SECOND = DRIVE_ROTATIONS_TO_METERS / 60.0;
+        public static final double ANGLE_MOTOR_GEAR_RATIO = 12.8 / 1.0; // 12.8:1
 
-    public static final int kEncoderCPR = 1024;
-    public static final double kWheelDiameterMeters = 0.15;
-    public static final double kDriveEncoderDistancePerPulse =
-        // Assumes the encoders are directly mounted on the wheel shafts
-        (kWheelDiameterMeters * Math.PI) / (double) kEncoderCPR;
+        /* note that these angle constant refer to the CTRE absolute encoder */
+        public static final double kAngleEncodeAnglePerRev = (Math.PI * 2);
+        public static final double kAngleEncodeVelocityPerRev = DRIVE_ROTATIONS_TO_METERS / 60.0;
 
-    public static final double kTurningEncoderDistancePerPulse =
-        // Assumes the encoders are on a 1:1 reduction with the module shaft.
-        (2 * Math.PI) / (double) kEncoderCPR;
+        public static final double kPModuleTurningController = 1;
 
-    /** Constants that apply to the whole drive train. */
-    public static final double TRACK_WIDTH = Units.inchesToMeters(24.0); // Width of the drivetrain measured from the middle of the wheels.
-    public static final double WHEEL_BASE = Units.inchesToMeters(24.0); // Length of the drivetrain measured from the middle of the wheels.
-    public static final double WHEEL_DIAMETER = Units.inchesToMeters(4);
-    public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
+        public static final double kPModuleDriveController = 1;
 
-    // SDS Mk4i L2 Drive Ratio = 6.75:1, Angle Ratio = 150/7:1
-    public static final double DRIVE_GEAR_RATIO = 6.75 / 1.0; // 6.75:1
-    public static final double DRIVE_ROTATIONS_TO_METERS = WHEEL_CIRCUMFERENCE / DRIVE_GEAR_RATIO;
-    public static final double DRIVE_RPM_TO_METERS_PER_SECOND = DRIVE_ROTATIONS_TO_METERS / 60.0;
-    public static final double ANGLE_MOTOR_GEAR_RATIO = 12.8 / 1.0; // 12.8:1
-    
-    /* note that these angle constant refer to the CTRE absolute encoder */
-    public static final double kAngleEncodeAnglePerRev = (Math.PI * 2);
-    public static final double kAngleEncodeVelocityPerRev = DRIVE_ROTATIONS_TO_METERS / 60.0;
+        /** Idle modes. */
+        public static final IdleMode DRIVE_IDLE_MODE = IdleMode.kBrake;
+        public static final IdleMode ANGLE_IDLE_MODE = IdleMode.kCoast;
 
-    public static final double kPModuleTurningController = 1;
+        /** Current limiting. */
+        public static final int DRIVE_CURRENT_LIMIT = 35;
+        public static final int ANGLE_CURRENT_LIMIT = 25;
 
-    public static final double kPModuleDriveController = 1;
+        /**
+         * Module specific constants.
+         * CanCoder offset is in DEGREES, not radians like the rest of the repo.
+         * This is to make offset slightly more accurate and easier to measure.
+         */
+        // Front Left Module
+        public static final SwerveModuleConstants kMOD_1_Constants = new SwerveModuleConstants(
+                1,
+                6,
+                7,
+                false,
+                false,
+                254.5 // 360 * 0.7069
+        );
 
-    /** Idle modes. */
-    public static final IdleMode DRIVE_IDLE_MODE = IdleMode.kBrake;
-    public static final IdleMode ANGLE_IDLE_MODE = IdleMode.kCoast;
+        // Front Right
+        public static final SwerveModuleConstants kMOD_2_Constants = new SwerveModuleConstants(
+                2,
+                4,
+                5,
+                true,
+                true,
+                152.0 // 360*0.4221
+        );
 
-    /** Current limiting. */
-    public static final int DRIVE_CURRENT_LIMIT = 35;
-    public static final int ANGLE_CURRENT_LIMIT = 25;
+        // Back Left
+        public static final SwerveModuleConstants kMOD_3_Constants = new SwerveModuleConstants(
+                3,
+                2,
+                3,
+                false,
+                false,
+                0.0
+        );
 
-    /** 
-     * Module specific constants.
-     * CanCoder offset is in DEGREES, not radians like the rest of the repo.
-     * This is to make offset slightly more accurate and easier to measure.
-     */
-    // Front Left Module
-    public static final SwerveModuleConstants kMOD_1_Constants = new SwerveModuleConstants(
-      1,
-      6,
-      7,
-      false,
-      false,
-      254.5 // 360 * 0.7069
-    );
+        // Back Right
+        public static final SwerveModuleConstants kMOD_4_Constants = new SwerveModuleConstants(
+                4,
+                1,
+                8,
+                true,
+                true,
+                0.0
+        );
+    }
 
-    // Front Right
-    public static final SwerveModuleConstants kMOD_2_Constants = new SwerveModuleConstants(
-      2,
-      4,
-      5,
-      true,
-      true,
-      152.0 // 360*0.4221 
-    );
+    public static final class OIConstants {
+        public static final int kDriverControllerPort = 0;
+        public static final int GYRO_RESET_BUTTON = XboxController.Button.kA.value;
+        public static final int ENCODER_RESET_BUTTON = XboxController.Button.kB.value;
+    }
 
-    // Back Left
-    public static final SwerveModuleConstants kMOD_3_Constants = new SwerveModuleConstants(
-      3,
-      2,
-      3,
-      false,
-      false,
-      0.0
-    );
+    public static final class AutoConstants {
+        public static final double kMaxSpeedMetersPerSecond = 3;
+        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+        public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+        public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
 
-    // Back Right
-    public static final SwerveModuleConstants kMOD_4_Constants = new SwerveModuleConstants(
-      4,
-      1,
-      8,
-      true,
-      true,
-      0.0
-    );
-  }
+        public static final double kPXController = 1;
+        public static final double kPYController = 1;
+        public static final double kPThetaController = 1;
 
-  public static final class OIConstants {
-    public static final int kDriverControllerPort = 0;
-    public static final int GYRO_RESET_BUTTON = XboxController.Button.kA.value;
-    public static final int ENCODER_RESET_BUTTON = XboxController.Button.kB.value;
-  }
-
-  public static final class AutoConstants {
-    public static final double kMaxSpeedMetersPerSecond = 3;
-    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
-    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
-    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
-
-    public static final double kPXController = 1;
-    public static final double kPYController = 1;
-    public static final double kPThetaController = 1;
-
-    // Constraint for the motion profiled robot angle controller
-    public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
-        new TrapezoidProfile.Constraints(
-            kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
-  }
+        // Constraint for the motion profiled robot angle controller
+        public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+                new TrapezoidProfile.Constraints(
+                        kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+    }
 }
--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -25,158 +25,158 @@ import frc.robot.Constants.ModuleConstants;
 import frc.robot.utils.SwerveModuleConstants;
 
 public class SwerveModule {
-  private SwerveModuleConstants module_constants;
+    private SwerveModuleConstants module_constants;
 
-  private final CANSparkMax m_driveMotor;
-  private final CANSparkMax m_turningMotor;
+    private final CANSparkMax m_driveMotor;
+    private final CANSparkMax m_turningMotor;
 
-  private final RelativeEncoder m_driveMotorEncoder;
-  private final RelativeEncoder m_turningMotorEncoder;
-  private final SparkMaxAbsoluteEncoder m_angleEncoder;
+    private final RelativeEncoder m_driveMotorEncoder;
+    private final RelativeEncoder m_turningMotorEncoder;
+    private final SparkMaxAbsoluteEncoder m_angleEncoder;
 
-  public double lastAngle;
+    public double lastAngle;
 
-  private final PIDController m_drivePIDController = new PIDController(ModuleConstants.kPModuleDriveController, 0, 0);
+    private final PIDController m_drivePIDController = new PIDController(ModuleConstants.kPModuleDriveController, 0, 0);
 
-  // Using a TrapezoidProfile PIDController to allow for smooth turning
-  private final ProfiledPIDController m_turningPIDController = new ProfiledPIDController(
-      ModuleConstants.kPModuleTurningController,
-      0,
-      0,
-      new TrapezoidProfile.Constraints(
-          ModuleConstants.kMaxModuleAngularSpeedRadiansPerSecond,
-          ModuleConstants.kMaxModuleAngularAccelerationRadiansPerSecondSquared));
-
-  /**
-   * Constructs a SwerveModule.
-   *
-   * @param module_constants The module constants (i.e. CAN IDs) for this specific module
-   */
-  public SwerveModule(SwerveModuleConstants module_constants) {
-    this.module_constants = module_constants;
-
-    m_driveMotor = new CANSparkMax(module_constants.driveMotorID, MotorType.kBrushless);
-    m_turningMotor = new CANSparkMax(module_constants.angleMotorID, MotorType.kBrushless);
-
-    m_driveMotorEncoder = m_driveMotor.getEncoder();
-    m_turningMotorEncoder = m_turningMotor.getEncoder();
-
-    m_angleEncoder = m_turningMotor.getAbsoluteEncoder(Type.kDutyCycle);
-
-
-    configureDevices();
-    lastAngle = getState().angle.getRadians();
-  }
-
-  /**
-   * Returns the current state of the module.
-   *
-   * @return The current state of the module.
-   */
-  public SwerveModuleState getState() {
-    double velocity = m_driveMotorEncoder.getVelocity();
-    Rotation2d rot = new Rotation2d(m_angleEncoder.getPosition());
-    return new SwerveModuleState(velocity, rot);
-  }
-
-  /**
-   * Returns the current position of the module.
-   *
-   * @return The current position of the module.
-   */
-  public SwerveModulePosition getPosition() {
-    double distance = m_driveMotorEncoder.getPosition();
-    Rotation2d rot = new Rotation2d(m_angleEncoder.getPosition());
-    return new SwerveModulePosition(distance, rot);
-  }
-
-  /**
-   * Sets the desired state for the module.
-   *
-   * @param desiredState Desired state with speed and angle.
-   */
-  public void setDesiredState(SwerveModuleState desiredState) {
-    // Optimize the reference state to avoid spinning further than 90 degrees
-    SwerveModuleState state = SwerveModuleState.optimize(desiredState, getState().angle);
-
-    // Calculate the drive output from the drive PID controller.
-    final double driveOutput = m_drivePIDController.calculate(m_driveMotorEncoder.getVelocity(), state.speedMetersPerSecond);
-
-    // Calculate the turning motor output from the turning PID controller.
-    final double turnOutput = m_turningPIDController.calculate(m_angleEncoder.getPosition(), state.angle.getRadians() 
-      - Units.degreesToRadians(module_constants.angleEncoderOffsetDegrees));
-
-    // Calculate the turning motor output from the turning PID controller.
-    m_driveMotor.set(driveOutput);
-    m_turningMotor.set(turnOutput);
-  }
-
-  /** Zeroes all the SwerveModule encoders. */
-  public void resetEncoders() {
-    m_driveMotorEncoder.setPosition(0.0);
-    m_turningMotorEncoder.setPosition(0.0);
-  }
-
-  /*
-  * Configuration values for the module hardware.  Specifically the motors and encoders. 
-  */
-  private void configureDevices() {
-    // Drive motor configuration:
-    //  - L2 NEO Motor connected to SParkMax
-    //  - SDS4i model L2 has a drive motor to wheel ratio of 6.75:1
-    // and an adjusted speed of 14.5 ft/sec with the NEO motors
-    //  - Outside wheel diameter = 4in
-    m_driveMotor.restoreFactoryDefaults();
-    m_driveMotor.clearFaults();
-    if (m_driveMotor.setIdleMode(IdleMode.kCoast) != REVLibError.kOk) {
-      SmartDashboard.putString("Drive Motor Idle Mode", "Error");
-    }
-
-    // Set the distance per pulse for the drive encoder. We can simply use the
-    // distance traveled for one rotation of the wheel divided by the encoder
-    // resolution.
-    m_driveMotorEncoder.setPositionConversionFactor(ModuleConstants.kDriveEncoderDistancePerPulse);
-
-    // Set whether drive encoder should be reversed or not
-    m_driveMotor.setInverted(module_constants.driveMotorReversed);
-
-    // Set the distance (in this case, angle) in radians per pulse for the turning
-    // encoder.
-    // This is the the angle through an entire rotation (2 * pi) divided by the
-    // encoder resolution.
-    m_turningMotorEncoder.setPositionConversionFactor(ModuleConstants.kTurningEncoderDistancePerPulse);
-
-    // Set whether turning encoder should be reversed or not
-    m_turningMotor.setInverted(module_constants.angleMotorReversed);
-
-    m_driveMotor.setInverted(module_constants.driveMotorReversed);
-    m_driveMotor.setIdleMode(Constants.ModuleConstants.DRIVE_IDLE_MODE);
-    m_driveMotorEncoder.setPositionConversionFactor(Constants.ModuleConstants.kDriveEncoderDistancePerPulse);
-    m_driveMotorEncoder.setPosition(0);
-
-    // Angle motor configuration.
-    // Neo Motor connected to SParkMax (all turn motors are reversed in the SDS 4i)
-    // The steering gear ration (from turning motor to wheel) is 150/7:1 = 21.43
-    m_turningMotor.restoreFactoryDefaults();
-    m_turningMotor.clearFaults();
-    if (m_turningMotor.setIdleMode(IdleMode.kCoast) != REVLibError.kOk) {
-      SmartDashboard.putString("Turn Motor Idle Mode", "Error");
-    }
-    m_turningMotor.setInverted(module_constants.angleMotorReversed);
-    m_turningMotor.setIdleMode(Constants.ModuleConstants.ANGLE_IDLE_MODE);
-    m_turningMotor.setSmartCurrentLimit(Constants.ModuleConstants.ANGLE_CURRENT_LIMIT);
+    // Using a TrapezoidProfile PIDController to allow for smooth turning
+    private final ProfiledPIDController m_turningPIDController = new ProfiledPIDController(
+            ModuleConstants.kPModuleTurningController,
+            0,
+            0,
+            new TrapezoidProfile.Constraints(
+                    ModuleConstants.kMaxModuleAngularSpeedRadiansPerSecond,
+                    ModuleConstants.kMaxModuleAngularAccelerationRadiansPerSecondSquared));
 
     /**
-     * CTRE Mag Encoder connected to the SparkMAX Absolute/Analog/PWM Duty Cycle
-     * input
-     * Native will ready 0.0 -> 1.0 for each revolution.
+     * Constructs a SwerveModule.
+     *
+     * @param module_constants The module constants (i.e. CAN IDs) for this specific module
      */
-    m_angleEncoder.setPositionConversionFactor(ModuleConstants.kAngleEncodeAnglePerRev);
-    m_angleEncoder.setPositionConversionFactor(Constants.ModuleConstants.kAngleEncodeAnglePerRev);
-    m_angleEncoder.setVelocityConversionFactor(Constants.ModuleConstants.kAngleEncodeAnglePerRev);
-    m_angleEncoder.setInverted(false);
+    public SwerveModule(SwerveModuleConstants module_constants) {
+        this.module_constants = module_constants;
 
-    m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
-  }
+        m_driveMotor = new CANSparkMax(module_constants.driveMotorID, MotorType.kBrushless);
+        m_turningMotor = new CANSparkMax(module_constants.angleMotorID, MotorType.kBrushless);
+
+        m_driveMotorEncoder = m_driveMotor.getEncoder();
+        m_turningMotorEncoder = m_turningMotor.getEncoder();
+
+        m_angleEncoder = m_turningMotor.getAbsoluteEncoder(Type.kDutyCycle);
+
+
+        configureDevices();
+        lastAngle = getState().angle.getRadians();
+    }
+
+    /**
+     * Returns the current state of the module.
+     *
+     * @return The current state of the module.
+     */
+    public SwerveModuleState getState() {
+        double velocity = m_driveMotorEncoder.getVelocity();
+        Rotation2d rot = new Rotation2d(m_angleEncoder.getPosition());
+        return new SwerveModuleState(velocity, rot);
+    }
+
+    /**
+     * Returns the current position of the module.
+     *
+     * @return The current position of the module.
+     */
+    public SwerveModulePosition getPosition() {
+        double distance = m_driveMotorEncoder.getPosition();
+        Rotation2d rot = new Rotation2d(m_angleEncoder.getPosition());
+        return new SwerveModulePosition(distance, rot);
+    }
+
+    /**
+     * Sets the desired state for the module.
+     *
+     * @param desiredState Desired state with speed and angle.
+     */
+    public void setDesiredState(SwerveModuleState desiredState) {
+        // Optimize the reference state to avoid spinning further than 90 degrees
+        SwerveModuleState state = SwerveModuleState.optimize(desiredState, getState().angle);
+
+        // Calculate the drive output from the drive PID controller.
+        final double driveOutput = m_drivePIDController.calculate(m_driveMotorEncoder.getVelocity(), state.speedMetersPerSecond);
+
+        // Calculate the turning motor output from the turning PID controller.
+        final double turnOutput = m_turningPIDController.calculate(m_angleEncoder.getPosition(), state.angle.getRadians()
+                - Units.degreesToRadians(module_constants.angleEncoderOffsetDegrees));
+
+        // Calculate the turning motor output from the turning PID controller.
+        m_driveMotor.set(driveOutput);
+        m_turningMotor.set(turnOutput);
+    }
+
+    /** Zeroes all the SwerveModule encoders. */
+    public void resetEncoders() {
+        m_driveMotorEncoder.setPosition(0.0);
+        m_turningMotorEncoder.setPosition(0.0);
+    }
+
+    /*
+     * Configuration values for the module hardware.  Specifically the motors and encoders.
+     */
+    private void configureDevices() {
+        // Drive motor configuration:
+        //  - L2 NEO Motor connected to SParkMax
+        //  - SDS4i model L2 has a drive motor to wheel ratio of 6.75:1
+        // and an adjusted speed of 14.5 ft/sec with the NEO motors
+        //  - Outside wheel diameter = 4in
+        m_driveMotor.restoreFactoryDefaults();
+        m_driveMotor.clearFaults();
+        if (m_driveMotor.setIdleMode(IdleMode.kCoast) != REVLibError.kOk) {
+            SmartDashboard.putString("Drive Motor Idle Mode", "Error");
+        }
+
+        // Set the distance per pulse for the drive encoder. We can simply use the
+        // distance traveled for one rotation of the wheel divided by the encoder
+        // resolution.
+        m_driveMotorEncoder.setPositionConversionFactor(ModuleConstants.kDriveEncoderDistancePerPulse);
+
+        // Set whether drive encoder should be reversed or not
+        m_driveMotor.setInverted(module_constants.driveMotorReversed);
+
+        // Set the distance (in this case, angle) in radians per pulse for the turning
+        // encoder.
+        // This is the the angle through an entire rotation (2 * pi) divided by the
+        // encoder resolution.
+        m_turningMotorEncoder.setPositionConversionFactor(ModuleConstants.kTurningEncoderDistancePerPulse);
+
+        // Set whether turning encoder should be reversed or not
+        m_turningMotor.setInverted(module_constants.angleMotorReversed);
+
+        m_driveMotor.setInverted(module_constants.driveMotorReversed);
+        m_driveMotor.setIdleMode(Constants.ModuleConstants.DRIVE_IDLE_MODE);
+        m_driveMotorEncoder.setPositionConversionFactor(Constants.ModuleConstants.kDriveEncoderDistancePerPulse);
+        m_driveMotorEncoder.setPosition(0);
+
+        // Angle motor configuration.
+        // Neo Motor connected to SParkMax (all turn motors are reversed in the SDS 4i)
+        // The steering gear ration (from turning motor to wheel) is 150/7:1 = 21.43
+        m_turningMotor.restoreFactoryDefaults();
+        m_turningMotor.clearFaults();
+        if (m_turningMotor.setIdleMode(IdleMode.kCoast) != REVLibError.kOk) {
+            SmartDashboard.putString("Turn Motor Idle Mode", "Error");
+        }
+        m_turningMotor.setInverted(module_constants.angleMotorReversed);
+        m_turningMotor.setIdleMode(Constants.ModuleConstants.ANGLE_IDLE_MODE);
+        m_turningMotor.setSmartCurrentLimit(Constants.ModuleConstants.ANGLE_CURRENT_LIMIT);
+
+        /**
+         * CTRE Mag Encoder connected to the SparkMAX Absolute/Analog/PWM Duty Cycle
+         * input
+         * Native will ready 0.0 -> 1.0 for each revolution.
+         */
+        m_angleEncoder.setPositionConversionFactor(ModuleConstants.kAngleEncodeAnglePerRev);
+        m_angleEncoder.setPositionConversionFactor(Constants.ModuleConstants.kAngleEncodeAnglePerRev);
+        m_angleEncoder.setVelocityConversionFactor(Constants.ModuleConstants.kAngleEncodeAnglePerRev);
+        m_angleEncoder.setInverted(false);
+
+        m_turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
+    }
 
 }
